b508cb7e14e1aa8149ce4732f9bbc31fbe6869d1
Joao F <joaopedrofavoretti@gmail.com>
2024-01-04 09:54:16 -0300

    remove nuxtjs.yml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 export default defineNuxtConfig({
   extends: 'content-wind',
+  app: {
+    baseURL: '/',
+    buildAssetsDir: 'assets',
+  },
   content: {
     highlight: {
       theme: 'one-dark-pro',
